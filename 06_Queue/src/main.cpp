3a4a1d49cbc5f12b6f9f8f68cc74e79cdb408e56
seoyyung <42532250+sy-project@users.noreply.github.com>
2024-11-08 16:38:55 +0900

    Add Queue

---
+++ 06_Queue/src/main.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int main(int argc, char* argv[])
+{
+    int i;
+    GQueue* queue = g_queue_new();
+
+    for(i = 0; i < argc; i++)
+    {
+        g_queue_push_head(queue, argv[i]);
+    }
+
+    while(!g_queue_is_empty(queue))
+    {
+        printf("%s\n", (char*)g_queue_pop_tail(queue));
+    }
+    return 0;
+}
